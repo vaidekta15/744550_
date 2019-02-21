eefe55bf0373339720f83c3d178197f185e7be97
ekta15.vaid <ekta15.vaid@gmail.com>
2019-02-21 08:51:39 -0500

    COMMIT

--- 744550_/js/main.js
+++ 744550_/js/main.js
@@ -1,1 +1,21 @@
 ﻿// Your code here!
+var bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven"
+    callType: "squawky",
+    quote: "Nevermore",
+    maxOffspring: 5,
+    noisy: true,
+    deadly: false
+};
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffspring: 3,
+    noisy: true,
+    deadly: true
+};
